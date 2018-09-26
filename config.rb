55f13117ea5bd64a161f34f3a102ef7d327f07e5
renshuki <renshuki@mail.com>
2018-09-26 17:19:40 +0200

    Make configuration apart from script

---
+++ config.rb
@@ -0,0 +1,16 @@
+class PhraseAppConfig
+
+# Your PhraseApp Token
+PHRASEAPP_TOKEN = "YOUR_PHRASEAPP_TOKEN"
+
+# PhraseApp Project ID
+PROJECT_ID = "YOUR_PROJECT_ID"
+
+# PhraseApp Locale ID
+LOCALE_ID = "YOUR_LOCALE_ID"
+
+# CSV File Path
+CSV_FILE_PATH = "./sample.csv"
+
+end
+
